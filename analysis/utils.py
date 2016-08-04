b7573e8c6bd181f42b7aa5f8d53adea2ffc10a81
Robin Andeer <robin.andeer@gmail.com>
2016-08-04 10:37:44 +0200

    handle analyses started multiple times

--- analysis/utils.py
+++ analysis/utils.py
@@ -3,8 +3,6 @@ from datetime import datetime
 
 from path import path
 
-from analysis.exc import MultipleFilesError
-
 
 def scan(root_dir):
     """Scan root for MIP analyses.
@@ -19,7 +17,8 @@ def get_sacctout(analysis_root):
     """Return the path to the sacct STDOUT file."""
     files = path(analysis_root).glob('bwa/info/Sacct_*.stdout.txt')
     if len(files) > 1:
-        raise MultipleFilesError(files)
+        # pick the "latest" file - increments with number in the filename
+        return files[-1]
     try:
         return files[0]
     except IndexError:
