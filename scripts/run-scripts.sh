8d3426d3c8fd355b699c059ce21ec1bc836792b0
Robin Andeer <robin.andeer@gmail.com>
2016-08-23 08:55:25 +0200

    update script starter!

--- scripts/run-scripts.sh
+++ scripts/run-scripts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
 display_usage() {
-    echo "Start Bash scripts one-by-one."
-    echo -e "\nUsage:\n$0 [list of scripts] \n"
+    echo "Start Bash scripts in a directory one-by-one."
+    echo -e "\nUsage:\n$0 [script directory] \n"
 }
 
 exe_script() {
@@ -11,16 +11,20 @@ exe_script() {
     bash "$temp_loc"
 }
 
-# if no root dir as argument
+# if no script dir as argument
 if [[ $# -eq 0 ]]; then
     display_usage
     exit 1
 fi
 
-for script in "$@"
+for script in "${1}"/*;
 do
-    echo "executing script: ${script}"
-    exe_script $script
+    filename=$(basename "$script")
+    extension="${filename##*.}"
+    if [ "$extension" == "sh" ]; then
+        echo "executing script: ${script}"
+        #exe_script $script
+    fi
 done
 
 exit 0
