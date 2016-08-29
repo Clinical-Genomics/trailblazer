a91309633d80172cc62a4cd82acd4d03c57dbac8
Robin Andeer <robin.andeer@gmail.com>
2016-08-29 16:01:34 +0200

    auto reload page 5 min past the hour

---
+++ trailblazer/server/static/app.js
@@ -0,0 +1,12 @@
+function tick() {
+    // get the mins of the current time
+    var mins = new Date().getMinutes();
+    if (mins == '5') {
+        // reload the page to get new data
+        location.reload();
+    } else {
+        console.log('Tick ' + mins);
+    }
+}
+
+setInterval(tick, 1000 * 60);
