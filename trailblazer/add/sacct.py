45fd584636a2ea28ed887d94cd9ae4cded5f7a6d
Robin Andeer <robin.andeer@gmail.com>
2016-09-29 15:57:24 +0200

    skip seconds column in sacct time calculation

--- trailblazer/add/sacct.py
+++ trailblazer/add/sacct.py
@@ -27,10 +27,8 @@ def time_to_sec(time_str):
     # parse out the hours and mins (skip seconds)
     hours_min_str = time_str.split(':')[:-1]
     time_parts = map(lambda val: int(round(float(val))), hours_min_str)
-    total_sec += time_parts[-1]               # seconds
-    total_sec += time_parts[-2] * 60          # minutes
-    if len(time_parts) == 3:
-        total_sec += time_parts[-3] * 60 * 60  # hours
+    total_sec += time_parts[-1] * 60          # minutes
+    total_sec += time_parts[-2] * 60 * 60     # hours
     return total_sec
 
 
