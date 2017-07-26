c11299641a2c74a642e8689c8f497440ae03c0f8
Robin Andeer <robin.andeer@gmail.com>
2017-07-26 10:15:27 +0200

    remove logic around naming of fastq files

--- trailblazer/mip/fastq.py
+++ trailblazer/mip/fastq.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-import datetime as dt
+from pathlib import Path
 import logging
-import os
 from typing import List
 
 log = logging.getLogger(__name__)
@@ -9,38 +8,12 @@ log = logging.getLogger(__name__)
 
 class FastqHandler:
 
-    def fastq_name(sample: str, lane: int, date: dt.datetime, flowcell: str, tile: int, read: int,
-                   undetermined: bool=False) -> str:
-        """Name a FASTQ file according to MIP conventions."""
-        template = "{lane}_{date}_{flowcell}-{tile}-{sample}_{read}.fastq.gz"
-        sample = f"Undetermined-{sample}" if undetermined else sample
-        mip_name = template.format(
-            lane=lane,
-            date=date.date().isoformat().replace('-', ''),
-            flowcell=flowcell,
-            tile=tile,
-            sample=sample,
-            read=read,
-        )
-        return mip_name
-
-    def link(self, family: str, sample: str, analysis_type: str, files: List[dict],
-             dry: bool=False):
+    def link(self, family: str, sample: str, analysis_type: str, files: List[str]):
         """Link FASTQ files for a sample."""
         root_dir = self.families_dir / family / analysis_type / sample / 'fastq'
-        if not dry:
-            root_dir.mkdir(parents=True, exist_ok=True)
+        root_dir.mkdir(parents=True, exist_ok=True)
         for fastq in files:
-            dest_name = self.fastq_name(
-                sample=sample,
-                lane=fastq['lane'],
-                date=fastq['date'],
-                flowcell=fastq['flowcell'],
-                tile=fastq['tile'],
-                read=fastq['read'],
-                undetermined=fastq['undetermined'],
-            )
-            dest_path = root_dir / dest_name
-            log.info(f"linking: {fastq['path']} -> {dest_path}")
-            if not dry:
-                os.link(fastq['path'], dest_path)
+            fastq_path = Path(fastq)
+            dest_path = root_dir / fastq_path.name
+            log.info(f"linking: {fastq_path} -> {dest_path}")
+            fastq_path.symlink_to(dest_path)
