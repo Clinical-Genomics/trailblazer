36bee7c9367cc12078aa64388408d195b54ee8ea
Robin Andeer <robin.andeer@gmail.com>
2017-08-01 10:54:45 +0200

    name fastq files according to MIP conventions

--- trailblazer/mip/fastq.py
+++ trailblazer/mip/fastq.py
@@ -8,14 +8,26 @@ log = logging.getLogger(__name__)
 
 class FastqHandler:
 
+    @staticmethod
+    def name_file(lane: int, flowcell: str, sample: str, read: int,
+                  undetermined: bool=False) -> str:
+        """Name a FASTQ file following MIP conventions."""
+        sample = f"{sample}-undetermined" if undetermined else sample
+        return f"{lane}_{flowcell}_{sample}_{read}.fastq.gz"
+
     def link(self, family: str, sample: str, analysis_type: str, files: List[str]):
         """Link FASTQ files for a sample."""
         root_dir = self.families_dir / family / analysis_type / sample / 'fastq'
         root_dir.mkdir(parents=True, exist_ok=True)
-        for fastq in files:
-            fastq_path = Path(fastq)
-            fastq_name = (f"{sample}_{fastq_path.name}" if sample not in fastq_path.name else
-                          fastq_path.name)
+        for fastq_data in files:
+            fastq_path = Path(fastq_data['path'])
+            fastq_name = self.name_file(
+                lane=fastq_data['lane'],
+                flowcell=fastq_data['flowcell'],
+                sample=sample,
+                read=fastq_data['read'],
+                undetermined=fastq_data['undetermined'],
+            )
             dest_path = root_dir / fastq_name
             if not dest_path.exists():
                 log.info(f"linking: {fastq_path} -> {dest_path}")
