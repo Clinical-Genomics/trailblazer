82062f9b3cf656c873cbb6eec025d7c7e6e4aee4
Robin Andeer <robin.andeer@gmail.com>
2017-11-15 09:17:03 +0100

    include analysis and duplicates in check output

--- trailblazer/cli/check.py
+++ trailblazer/cli/check.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 import click
@@ -5,6 +6,8 @@ import ruamel.yaml
 from tabulate import tabulate
 
 from trailblazer.mip import files
+
+LOG = logging.getLogger(__name__)
 
 
 @click.command()
@@ -14,12 +17,12 @@ def check(context: click.Context, family: str):
     """Delete an analysis log from the database."""
     analysis_obj = context.obj['store'].analyses(family=family).first()
     if analysis_obj is None:
-        print(click.style('no analysis found', fg='yellow'))
+        LOG.error('no analysis found')
         context.abort()
 
     config_path = Path(analysis_obj.config_path)
     if not config_path.exists():
-        print(click.style(f"analysis config not found: {config_path}", fg='red'))
+        LOG.error(f"analysis config not found: {config_path}")
         context.abort()
     config_raw = ruamel.yaml.safe_load(config_path.open())
     config_data = files.parse_config(config_raw)
@@ -29,17 +32,27 @@ def check(context: click.Context, family: str):
 
     qcmetrics_path = Path(sampleinfo_data['qcmetrics_path'])
     if not qcmetrics_path.exists():
-        print(click.style(f"qc metrics not found: {str(qcmetrics_path)}", fg='red'))
+        LOG.error(f"qc metrics not found: {str(qcmetrics_path)}")
         context.abort()
     qcmetrics_raw = ruamel.yaml.safe_load(qcmetrics_path.open())
     qcmetrics_data = files.parse_qcmetrics(qcmetrics_raw)
 
-    with Path(sampleinfo_data['peddy']['sex_check']).open() as sexcheck_handle:
-        peddy_data = files.parse_peddy_sexcheck(sexcheck_handle)
-
-    samples = {'sample': [], 'ped': [], 'chanjo': [], 'peddy': [], 'plink': []}
+    samples = {
+        'sample': [],
+        'type': [],
+        'ped': [],
+        'chanjo': [],
+        'peddy': [],
+        'plink': [],
+        'duplicates': [],
+    }
     for sample_data in sampleinfo_data['samples']:
+        LOG.debug(f"{sample_data['id']}: parse analysis config")
         samples['sample'].append(sample_data['id'])
+        samples['type'].append(sample_data['type'])
+
+    for sample_data in sampleinfo_data['samples']:
+        LOG.debug(f"{sample_data['id']}: parse sample info")
         samples['ped'].append(sample_data['sex'])
 
         with Path(sample_data['chanjo_sexcheck']).open() as chanjo_handle:
@@ -50,11 +63,22 @@ def check(context: click.Context, family: str):
         samples['chanjo'].append(f"{predicted_sex} ({xy_ratio:.3f})")
 
     for sample_data in qcmetrics_data['samples']:
+        LOG.debug(f"{sample_data['id']}: parse qc metrics")
         samples['plink'].append(sample_data['plink_sex'])
+        duplicates_percent = sample_data['duplicates'] * 100
+        samples['duplicates'].append(f"{duplicates_percent}%")
 
-    for sample_id in samples['sample']:
-        predicted_sex = peddy_data[sample_id]['predicted_sex']
-        het_ratio = peddy_data[sample_id]['het_ratio']
-        samples['peddy'].append(f"{predicted_sex} ({het_ratio})")
+    peddy_path = Path(sampleinfo_data['peddy']['sex_check'])
+    if peddy_path.exists():
+        with peddy_path.open() as sexcheck_handle:
+            peddy_data = files.parse_peddy_sexcheck(sexcheck_handle)
+
+        for sample_id in samples['sample']:
+            LOG.debug(f"{sample_id}: parse peddy")
+            predicted_sex = peddy_data[sample_id]['predicted_sex']
+            het_ratio = peddy_data[sample_id]['het_ratio']
+            samples['peddy'].append(f"{predicted_sex} ({het_ratio})")
+    else:
+        LOG.warning(f"missing peddy output: {peddy_path}")
 
     print(tabulate(samples, headers='keys', tablefmt='psql'))
