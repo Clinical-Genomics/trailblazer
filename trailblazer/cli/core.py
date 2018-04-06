749ecaa30857d9b9a729716efbeff19a26b97455
Emma Sernstad <emma.sernstad@scilifelab.se>
2018-04-06 14:58:39 +0200

    test

--- trailblazer/cli/core.py
+++ trailblazer/cli/core.py
@@ -74,14 +74,15 @@ def log_cmd(context, sampleinfo, sacct, quiet, config):
 @click.option('-p', '--priority', type=click.Choice(['low', 'normal', 'high']), default='normal')
 @click.option('-d', '--dryrun', is_flag=True, help='only generate SBATCH scripts')
 @click.option('--command', is_flag=True, help='only show the MIP command')
+@click.option('--startfrom', help='start the pipeline from this program')
 @click.argument('family', required=False)
 @click.pass_context
-def start(context, mip_config, email, priority, dryrun, command, family):
+def start(context, mip_config, email, priority, dryrun, command, startfrom, family):
     """Start a new analysis."""
     mip_cli = MipCli(context.obj['script'])
     mip_config = mip_config or context.obj['mip_config']
     email = email or environ_email()
-    kwargs = dict(config=mip_config, family=family, priority=priority, email=email, dryrun=dryrun)
+    kwargs = dict(config=mip_config, family=family, priority=priority, email=email, dryrun=dryrun, startfrom=startfrom)
     if command:
         mip_command = mip_cli.build_command(**kwargs)
         click.echo(' '.join(mip_command))
