aabcc7521208be447692add63a8c847e23ec3409
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-04-25 15:20:06 +0200

    pep8 refactor

--- trailblazer/mip/trending.py
+++ trailblazer/mip/trending.py
@@ -3,6 +3,7 @@
 
 from trailblazer.mip import files
 
+
 def parse_mip_analysis(mip_config_raw: dict, qcmetrics_raw: dict, sampleinfo_raw: dict) -> dict:
     """Parse the output analysis files from MIP for adding info
     to trend database
@@ -46,18 +47,17 @@ def parse_mip_analysis(mip_config_raw: dict, qcmetrics_raw: dict, sampleinfo_raw
 
     ## Add sample level info from qc_metric file
     for sample_data in qcmetrics_data['samples']:
-
         ## Add duplicate reads
         duplicates_percent = sample_data['duplicates'] * 100
         outdata['duplicates'][sample_data['id']] = duplicates_percent
 
         ## Add at and gc droputs
-        #outdata['at_dropout'][sample_data['id']] = sample_data['at_dropout']
-        #outdata['gc_dropout'][sample_data['id']] = sample_data['gc_dropout']
+        # outdata['at_dropout'][sample_data['id']] = sample_data['at_dropout']
+        # outdata['gc_dropout'][sample_data['id']] = sample_data['gc_dropout']
 
         ## Add insert size metrics
-        #outdata['median_insert_size'][sample_data['id']] = sample_data['median_insert_size']
-        #outdata['insert_size_standard_deviation'][sample_data['id']] = sample_data[
+        # outdata['median_insert_size'][sample_data['id']] = sample_data['median_insert_size']
+        # outdata['insert_size_standard_deviation'][sample_data['id']] = sample_data[
         # 'insert_size_standard_deviation']
 
         ## Add mapped reads
