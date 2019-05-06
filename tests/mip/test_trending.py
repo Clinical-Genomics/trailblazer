fd34347f4e9711f2c6a1cae9527151a5a79b307a
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2019-05-06 10:38:35 +0200

    Add SV rank model.

--- tests/mip/test_trending.py
+++ tests/mip/test_trending.py
@@ -26,6 +26,7 @@ def test_parse_mip_analysis(files_raw) -> dict:
         'genome_build': 'GRCh37',
         'mip_version': 'v6.0.0',
         'rank_model_version': '1.20'
+        'sv_rank_model_version': '1.2'
     }
 
     # Check returns from def
--- trailblazer/mip/files.py
+++ trailblazer/mip/files.py
@@ -52,6 +52,7 @@ def parse_sampleinfo(data: dict) -> dict:
         'family': data['family'],
         'genome_build': genome_build_str,
         'rank_model_version': data['program']['genmod']['rank_model']['version'],
+        'sv_rank_model_version': data['program']['genmod']['sv_rank_model']['version']
         'is_finished': True if data['analysisrunstatus'] == 'finished' else False,
         'pedigree_path': data['pedigree_minimal'],
         'peddy': {
