50c0a1bf8a7f13240eca8839be0bc36d3d0659b4
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-07-29 09:33:11 +0200

    feat<mip7.1>: Mip7 command (#56) to start mip
    * Change to use mip7 CLI

--- trailblazer/mip/start.py
+++ trailblazer/mip/start.py
@@ -8,22 +8,15 @@ from trailblazer.exc import MipStartError
 LOG = logging.getLogger(__name__)
 
 CLI_OPTIONS = {
-    'family': {'option': '--family_id'},
     'config': {'option': '--config_file'},
     'priority': {'option': '--slurm_quality_of_service'},
     'email': {'option': '--email'},
     'base': {'option': '--cluster_constant_path'},
-    'dryrun': {
-        'option': '--dry_run_all',
-        'default': '2',
-    },
-    'gene_list': {'option': '--vcfparser_select_file'},
-    'max_gaussian': {
-        'option': '--gatk_variantrecalibration_snv_max_gaussians',
-        'default': '1',
-    },
+    'dryrun': {'option': '--dry_run_all'},
+    'gene_list': {'option': '--vcfparser_slt_fl'},
+    'max_gaussian': {'option': '--gatk_varrecal_snv_max_gau'},
     'skip_evaluation': {'option': '--qccollect_skip_evaluation'},
-    'start_with': {'option': '--start_with_program'},
+    'start_with': {'option': '--start_with_recipe'},
 }
 
 
@@ -31,13 +24,14 @@ class MipCli(object):
 
     """Wrapper around MIP command line interface."""
 
-    def __init__(self, script):
+    def __init__(self, script, pipeline):
         """Initialize MIP command line interface."""
         self.script = script
+        self.pipeline = pipeline
 
-    def __call__(self, config, family, **kwargs):
+    def __call__(self, config, case, **kwargs):
         """Execute the pipeline."""
-        command = self.build_command(config, family=family, **kwargs)
+        command = self.build_command(case, config, **kwargs)
         LOG.debug(' '.join(command))
         process = self.execute(command)
         process.wait()
@@ -45,23 +39,26 @@ class MipCli(object):
             raise MipStartError('error starting analysis, check the output')
         return process
 
-    def build_command(self, config, **kwargs):
+    def build_command(self, case, config, **kwargs):
         """Builds the command to execute MIP."""
-        command = ['perl', self.script, CLI_OPTIONS['config']['option'], config]
+        command = [self.script, self.pipeline, case, CLI_OPTIONS['config']['option'], config]
         for key, value in kwargs.items():
             # enable passing in flags as "False" - shouldn't add command
             if value:
                 command.append(CLI_OPTIONS[key]['option'])
-                if value is True:
-                    command.append(CLI_OPTIONS[key].get('default', '1'))
-                else:
+
+                # append value for non-flags
+                if value is not True:
                     command.append(value)
         return command
 
-
-
     def execute(self, command):
         """Start a new MIP run."""
+
+        """
+        Remove the default SIGPIPE handler
+        https://blog.nelhage.com/2010/02/a-very-subtle-bug/
+        """
         process = subprocess.Popen(
             command,
             preexec_fn=lambda: signal.signal(signal.SIGPIPE, signal.SIG_DFL)
