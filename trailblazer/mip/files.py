337e7f0e1cc6b9f4a2b9bf7bc6d357e2da2b02fc
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-09-10 11:55:45 +0200

    reintroduce sv_rank_model middle key

--- trailblazer/mip/files.py
+++ trailblazer/mip/files.py
@@ -53,7 +53,7 @@ def parse_sampleinfo(data: dict) -> dict:
         'case': data['case'],
         'genome_build': genome_build_str,
         'rank_model_version': data['recipe']['genmod']['rank_model']['version'],
-        'sv_rank_model_version': data['recipe']['sv_genmod']['version'],
+        'sv_rank_model_version': data['recipe']['sv_genmod']['sv_rank_model']['version'],
         'is_finished': True if data['analysisrunstatus'] == 'finished' else False,
         'pedigree_path': data['pedigree_minimal'],
         'peddy': {
