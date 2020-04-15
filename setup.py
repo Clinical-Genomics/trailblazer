fed2457337574e21b2f59573036da7fdcc73d8c8
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-04-15 08:14:19 +0200

    Bump version: 6.1.0 → 6.1.1

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on
     # single-sourcing the version across setup.py and the project code,
     # see http://packaging.python.org/en/latest/tutorial.html#version
-    version='6.1.0',
+    version='6.1.1',
 
     description=('Track MIP analyses.'),
     long_description=__doc__,
