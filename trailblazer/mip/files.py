63d6154a2a0fe9d25a71be30792181fbb4d17c84
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-08-20 08:05:53 +0200

    fix(case_id): Added fall back to family_id if case_id could not be found in mip_config

--- trailblazer/mip/files.py
+++ trailblazer/mip/files.py
@@ -17,7 +17,7 @@ def parse_config(data: dict) -> dict:
     """
     return {
         'email': data.get('email'),
-        'case': data['case_id'],
+        'case': data['case_id'] if 'case_id' in data else data['family_id'],
         'samples': [{
             'id': sample_id,
             'type': analysis_type,
