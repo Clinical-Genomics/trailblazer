fdad91607e9e80bee29e8f5430dd09ec85e5996b
Henrik Stranneheim <henrik.stranneheim@scilifelab.se>
2020-08-25 16:38:10 +0200

    Feat(API): Methods to check status of analysis (#104)
    * feat(refactor): is_running
    
    * feat(WIP):
    
    * feat(TEMP_STATUS): Renamed TEMP_STATUS to ONGOING_STATUS
    
    * feat(analysis): Added def and refactor
    
    * feat(refactor):
    
    * feat(is_completed): Added is_completed status
    
    * feat(get_latest_analysis): Added sub and test
    
    * fix(test):
    
    * code format with black
    
    Co-authored-by: Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>

--- trailblazer/constants.py
+++ trailblazer/constants.py
@@ -1,2 +1,4 @@
-TEMP_STATUSES = ('pending', 'running')
-DEFAULT_CAPTURE_KIT = 'agilent_sureselect_cre.v1'
+DEFAULT_CAPTURE_KIT = "agilent_sureselect_cre.v1"
+COMPLETED_STATUS = "completed"
+FAILED_STATUS = "failed"
+ONGOING_STATUSES = ("pending", "running")
