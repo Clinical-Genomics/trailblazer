838d6e5aa15eb8fd350505fe7e4197e5f4dd0ab1
Mropat <mariaropat@gmail.com>
2020-10-27 13:34:00 +0100

    tweak update logic

--- trailblazer/store/api.py
+++ trailblazer/store/api.py
@@ -270,7 +270,7 @@ class BaseHandler:
                 slurm_id=val.get("id"),
                 name=val.get("step"),
                 status=val.get("status").lower(),
-                started_at=parse_datestr(val.get("started")) if val.get("started") else None,
+                started_at=val.get("started") if val.get("started") else None,
                 elapsed=int(
                     (parse_datestr(val.get("elapsed", "0:0:0")) - parse_datestr("0:0:0")).seconds
                 ),
