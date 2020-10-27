32435dd7752a8860a0c42b10473d4837aa4c94cd
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-10-27 08:32:47 +0100

    format with black

--- trailblazer/cli/core.py
+++ trailblazer/cli/core.py
@@ -118,20 +118,23 @@ def user(context, name, email):
 
 
 @base.command()
-@click.option('-j', '--jobs', is_flag=True, help='only print job ids')
-@click.argument('analysis_id', type=int, required=False)
-@click.option('-c', '--case', 'case_id', type=str, help="cancel latest running analysis of case")
+@click.option("-j", "--jobs", is_flag=True, help="only print job ids")
+@click.argument("analysis_id", type=int, required=False)
+@click.option("-c", "--case", "case_id", type=str, help="cancel latest running analysis of case")
 @click.pass_context
 def cancel(context, jobs, analysis_id=None, case_id=None):
 
     """Cancel all jobs in a run."""
     if analysis_id:
-        analysis_obj = context.obj['store'].analysis(analysis_id)
+        analysis_obj = context.obj["store"].analysis(analysis_id)
     elif case_id:
-        analyses = context.obj['store'].analyses(family=case_id, status="running")
+        analyses = context.obj["store"].analyses(family=case_id, status="running")
         if not analyses or not len(analyses.all()) == 1:
-            click.echo(click.style(f'{len(analyses.all())} running analyses found, 1 expected',
-                                   fg='yellow'))
+            click.echo(
+                click.style(
+                    f"{len(analyses.all())} running analyses found, 1 expected", fg="yellow"
+                )
+            )
             context.abort()
         analysis_obj = analyses.first()
 
