c1e33e1ee66c682f5e145f0b8e991ae62a007fa9
Mropat <mariaropat@gmail.com>
2020-10-29 15:33:43 +0100

    Bump version: 8.1.2 → 9.0.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="8.1.2",
+    version="9.0.0",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
