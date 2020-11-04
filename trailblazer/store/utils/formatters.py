5218b94aea0a35ad9bac83f2a6f2a69d115ba9d0
Mropat <mariaropat@gmail.com>
2020-11-04 16:26:40 +0100

    Parsers for jobs

---
+++ trailblazer/store/utils/formatters.py
@@ -0,0 +1,13 @@
+def transform_mip_job_name(job_name: str) -> str:
+    return "_".join(job_name.split("_")[:-1])
+
+
+def transform_balsamic_job_name(job_name: str) -> str:
+    return job_name.split(".")[2]
+
+
+formatter_map = {
+    "MIP-DNA": transform_mip_job_name,
+    "MIP-RNA": transform_mip_job_name,
+    "BALSAMIC": transform_balsamic_job_name,
+}
