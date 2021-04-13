8248e0f8a4fe8cc1fb60683746f75668f75c3e32
Mropat <mariaropat@gmail.com>
2021-04-13 13:49:40 +0200

    Bump version: 10.1.0 → 10.1.1

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="10.1.0",
+    version="10.1.1",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
