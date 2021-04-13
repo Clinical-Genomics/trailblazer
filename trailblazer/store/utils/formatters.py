71241aabebe6691079a3dce85c7e64d68d1fce71
Maria Ropat <35531751+Mropat@users.noreply.github.com>
2021-04-13 13:34:35 +0200

    Feat mutant formatting (#162)
    * format mutant jobs
    
    * handle if job formatting changes

--- trailblazer/store/utils/formatters.py
+++ trailblazer/store/utils/formatters.py
@@ -1,9 +1,23 @@
 def transform_mip_job_name(job_name: str) -> str:
-    return "_".join(job_name.split("_")[:-1])
+    try:
+
+        return "_".join(job_name.split("_")[:-1])
+    except Exception:
+        return job_name
 
 
 def transform_balsamic_job_name(job_name: str) -> str:
-    return job_name.split(".")[2]
+    try:
+        return job_name.split(".")[2]
+    except Exception:
+        return job_name
+
+
+def transform_mutant_job_name(job_name: str) -> str:
+    try:
+        return job_name.split("_")[2]
+    except Exception:
+        return job_name
 
 
 def transform_undefined(job_name: str) -> str:
@@ -14,4 +28,5 @@ formatter_map = {
     "MIP-DNA": transform_mip_job_name,
     "MIP-RNA": transform_mip_job_name,
     "BALSAMIC": transform_balsamic_job_name,
+    "SARS-COV-2": transform_mutant_job_name,
 }
