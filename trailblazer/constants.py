322d107ec45c5949562628cd072d3ff41ff00727
Maria Ropat <35531751+Mropat@users.noreply.github.com>
2021-10-19 08:43:49 +0200

    Additonal types (#177)(patch)
    ### Added
    * Job status: completing to allowed statuses

--- trailblazer/constants.py
+++ trailblazer/constants.py
@@ -1,12 +1,12 @@
 COMPLETED_STATUS = "completed"
 FAILED_STATUS = "failed"
-ONGOING_STATUSES = ("pending", "running", "error")
-STARTED_STATUSES = ["completed", "failed", "pending", "running", "error"]
-SLURM_NORMAL_CATEGORIES = ("completed", "running", "pending")
+ONGOING_STATUSES = ("pending", "running", "error", "completing")
+STARTED_STATUSES = ["completed", "failed", "pending", "running", "error", "completing"]
+SLURM_NORMAL_CATEGORIES = ("completed", "running", "pending", "completing")
 SLURM_FAILED_CATEGORIES = ("failed", "cancelled", "timeout")
-SLURM_ACTIVE_CATEGORIES = ("running", "pending")
+SLURM_ACTIVE_CATEGORIES = ("running", "pending", "completing")
 
-STATUS_OPTIONS = ("pending", "running", "completed", "failed", "error", "canceled")
+STATUS_OPTIONS = ("pending", "running", "completed", "failed", "error", "canceled", "completing")
 JOB_STATUS_OPTIONS = SLURM_NORMAL_CATEGORIES + SLURM_FAILED_CATEGORIES
 PRIORITY_OPTIONS = ("low", "normal", "high")
 TYPES = ("wes", "wgs", "rna", "tgs", "other")
