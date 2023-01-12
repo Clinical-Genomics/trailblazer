65336aeeb316e2bd813a6787e7d3f9e2feb379ad
GustavLagneborgScilifelab <107848458+GustavLagneborgScilifelab@users.noreply.github.com>
2023-01-12 14:58:24 +0100

    considering existing comments

--- trailblazer/store/api.py
+++ trailblazer/store/api.py
@@ -264,9 +264,11 @@ class BaseHandler:
 
     def add_comment(self, case_id: str, comment: str):
         analysis_obj: Analysis = self.get_latest_analysis(case_id=case_id)
-        analysis_obj.comment: str = comment + analysis_obj.comment if analysis_obj.comment else ""
-
+        analysis_obj.comment: str = (
+            " ".join([analysis_obj.comment, comment]) if analysis_obj.comment else comment
+        )
         self.commit()
+
         LOG.info(f"Adding comment {comment} to analysis {analysis_obj.family}")
 
     def delete_analysis(self, analysis_id: int, force: bool = False) -> None:
