fb980214c69fbc1dcaecc3654a557b127ce94ee3
Eva Caceres <eva.caceres@scilifelab.se>
2023-04-04 12:20:45 +0200

    Add tower tasks

--- trailblazer/store/executors.py
+++ trailblazer/store/executors.py
@@ -21,26 +21,26 @@ class TowerProcess:
         self.process: dict = process
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.process.get("process")
 
     @property
-    def status(self):
+    def status(self) -> str:
         for status_flag in PROCESS_STATUS.keys():
             if self.process.get(status_flag, 0):
                 return PROCESS_STATUS.get(status_flag)
         return TrailblazerStatus.ERROR
 
     @property
-    def date_created(self):
+    def date_created(self) -> str:
         return self.process.get("dateCreated")
 
     @property
-    def last_updated(self):
+    def last_updated(self) -> str:
         return self.process.get("lastUpdated")
 
     @property
-    def time_since_creation(self):
+    def time_since_creation(self) -> str:
         """Returns time elapsed from creation to last update."""
         pass  # TODO: self.last_update - self.date_created
 
@@ -51,6 +51,47 @@ class TowerProcess:
             return True
         else:
             return False
+
+
+class TowerTask:
+    """Class for Tower Task."""
+
+    def __init__(self, task: dict):
+        self.task: dict = task
+
+    @property
+    def name(self) -> str:
+        return self.task.get("process")
+
+    @property
+    def slurm_id(self) -> str:
+        return self.task.get("nativeId", None)
+
+    @property
+    def status(self) -> str:
+        return TOWER_STATUS.get(self.task.get("status"))
+
+    @property
+    def date_created(self) -> str:
+        return self.task.get("dateCreated")
+
+    @property
+    def last_updated(self) -> str:
+        return self.task.get("lastUpdated")
+
+    @property
+    def start(self) -> str:
+        return None if self.task.get("start") == "null" else self.task.get("start")
+
+    @property
+    def duration(self) -> str:
+        """Returns duration in seconds."""
+        return self.task.get("duration")
+
+    @property
+    def is_complete(self) -> bool:
+        """Returns if the process succeded."""
+        return self.status == TrailblazerStatus.COMPLETED.value
 
 
 class ExecutorAPI:
@@ -65,7 +106,7 @@ class TowerAPI(ExecutorAPI):
     """Handles communication with tower."""
 
     @property
-    def executor_id(self):
+    def executor_id(self) -> int:
         """Returns executor id"""
         id_dict = safe_load(open(self.id_file))
         return id_dict.get(next(iter(id_dict)))[0]
@@ -83,26 +124,25 @@ class TowerAPI(ExecutorAPI):
     @property
     def is_pending(self) -> bool:
         """Returns True if workflow has not started running. Otherwise returns False."""
-        if self.status == TrailblazerStatus.PENDING.value:
-            return True
-        else:
-            return False
+        return self.status == TrailblazerStatus.PENDING.value
 
     @property
     def is_complete(self) -> bool:
         """Returns True if workflow has completed. Otherwise returns False."""
-        if self.status == TrailblazerStatus.COMPLETED.value:
-            return True
-        else:
-            return False
+        return self.status == TrailblazerStatus.COMPLETED.value
 
     @property
-    def processes(self) -> dict:
-        """Returns jobs dictionary."""
+    def processes(self) -> List[TowerProcess]:
+        """Returns processes."""
         return [
             TowerProcess(process=process)
             for process in self.response["progress"]["processesProgress"]
         ]
+
+    @property
+    def tasks(self) -> List[TowerTask]:
+        """Returns tasks."""
+        return [TowerTask(task=task["task"]) for task in self.tasks_response["tasks"]]
 
     @property
     def total_jobs(self) -> int:
@@ -126,39 +166,35 @@ class TowerAPI(ExecutorAPI):
         else:
             return int(self.succeeded_jobs * 100.0 / self.total_jobs)
 
+    # def get_jobs(self, analysis_id: str) -> List[Job]:
+    #     """Returns a list of jobs associated to a workflow."""
+    #     return [
+    #         self._get_job(process=process, analysis_id=analysis_id) for process in self.processes
+    #     ]
+    #
+    # def _get_job(self, process: TowerProcess, analysis_id: str) -> dict:
+    #     """Format a job with required information."""
+    #     return models.Job(
+    #         analysis_id=analysis_id,
+    #         name=process.name,
+    #         status=process.status,
+    #         started_at=process.date_created,
+    #         elapsed=process.time_since_creation,
+    #     )
+
     def get_jobs(self, analysis_id: str) -> List[Job]:
         """Returns a list of jobs associated to a workflow."""
-        return [
-            self._get_job(process=process, analysis_id=analysis_id) for process in self.processes
-        ]
+        return [self._get_job(task=task, analysis_id=analysis_id) for task in self.tasks]
 
-    # @property
-    # def jobs_dataframe(self) -> pd.DataFrame:
-    #     """Returns a formatted jobs dataframe."""
-    #     return pd.DataFrame([self._format_job(job=job) for job in self.processes()])
-
-    # def _format_job(self, job: dict) -> dict:
-    #     """Format a job with required information."""
-    #     process = TowerProcess(job)
-    #     return {
-    #         "id": 000,  # TODO FIGURE OUT
-    #         "analysis_id": self.id,
-    #         "slurm_id": None,
-    #         "name": process.name,
-    #         "context": None,
-    #         "elapsed": process.time_since_creation,
-    #         "started_at": process.date_created,
-    #         "status": process.status,
-    #     }
-
-    def _get_job(self, process: TowerProcess, analysis_id: str) -> dict:
+    def _get_job(self, task: TowerTask, analysis_id: str) -> Job:
         """Format a job with required information."""
         return models.Job(
             analysis_id=analysis_id,
-            name=process.name,
-            status=process.status,
-            started_at=process.date_created,
-            elapsed=process.time_since_creation,
+            slurm_id=task.slurm_id,
+            name=task.name,
+            status=task.status,
+            started_at=task.start,
+            elapsed=task.duration / 60,
         )
 
     def query(self) -> dict:
