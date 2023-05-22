c1010929613590f17dd23235b6b0f473a7f5ec66
Henrik Stranneheim <henrik.stranneheim@scilifelab.se>
2023-05-22 10:14:02 +0200

    feat(update): Python to 3.7 for Docker and Pypi (#200) (major)
    ### Changed
    
    - Update Python to 3.7 for Docker and Pypi
    - Update actions versions for checkout and Python
    - Remove clinical-db install action

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Environment :: Console",
     ],
 )
