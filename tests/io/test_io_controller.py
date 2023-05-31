5eaf4ef6ef5ef8c025db94a0104d9e7c6b2ce538
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-05-31 12:44:51 +0200

    feat(refactor):

--- tests/io/test_io_controller.py
+++ tests/io/test_io_controller.py
@@ -4,16 +4,16 @@ from trailblazer.constants import FileFormat
 from trailblazer.io.controller import ReadFile
 
 
-def test_get_content_from_file(sample_data_path: Path):
+def test_get_content_from_file(example_yaml_path: Path):
     """
     Tests get content from file.
     """
     # GIVEN a YAML file
 
     # WHEN reading the YAML file
-    sample_data_content: dict = ReadFile.get_content_from_file(
-        file_format=FileFormat.YAML, file_path=sample_data_path
+    yaml_content: dict = ReadFile.get_content_from_file(
+        file_format=FileFormat.YAML, file_path=example_yaml_path
     )
 
     # Then assert a dict is returned
-    assert isinstance(sample_data_content, dict)
+    assert isinstance(yaml_content, dict)
--- tests/io/test_io_yaml.py
+++ tests/io/test_io_yaml.py
@@ -3,17 +3,21 @@ from pathlib import Path
 from trailblazer.io.yaml import read_yaml
 
 
-def test_get_content_from_file(sample_data_path: Path):
+def test_get_content_from_file(example_yaml_path: Path):
     """
     Tests reading a YAML file.
     """
     # GIVEN a YAML file
 
     # WHEN reading the YAML file
-    sample_data_content: dict = read_yaml(file_path=sample_data_path)
+    yaml_content: dict = read_yaml(file_path=example_yaml_path)
 
     # Then assert a dict is returned
-    assert isinstance(sample_data_content, dict)
+    assert isinstance(yaml_content, dict)
 
-    # Then assert we can access returned dict
-    assert sample_data_content.get("users")
+    # Then assert content is unchanged
+    assert yaml_content == {
+        "users": [
+            {"name": "Paul Anderson"},
+        ],
+    }
