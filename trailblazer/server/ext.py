8c80c1cc66f467c7c8740eaf55036622814a246d
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-06-08 11:19:33 +0200

    fix(import):

--- trailblazer/server/ext.py
+++ trailblazer/server/ext.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
 from flask_alchy import Alchy
 
-from trailblazer.store import BaseHandler, models
+from trailblazer.store.api import BaseHandler
+from trailblazer.store import models
 
 
 class TrailblazerAlchy(Alchy, BaseHandler):
