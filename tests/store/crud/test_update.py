76aed7375fc61fa87e00526cce2e254d57f081bf
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-06-12 09:20:53 +0200

    fix(typo):

--- tests/store/crud/test_update.py
+++ tests/store/crud/test_update.py
@@ -4,14 +4,14 @@ from tests.mocks.store_mock import MockStore
 from trailblazer.store.models import Info
 
 
-def test_set_latest_update_date(store: MockStore):
-    """Test setting the info table latest updated date."""
+def test_update_latest_update_date(store: MockStore):
+    """Test updating the info table latest updated date."""
 
     # GIVEN a store which is empty apart from the initialized info entry
     assert store._get_query(table=Info).first().updated_at is None
 
     # WHEN setting the last updated date
-    store.set_latest_update_date()
+    store.update_latest_update_date()
     info: Info = store._get_query(table=Info).first()
 
     # THEN it should update info entry with the current date
--- trailblazer/store/crud/update.py
+++ trailblazer/store/crud/update.py
@@ -1,4 +1,4 @@
-import datetime as dt
+from datetime import datetime
 
 from trailblazer.store.base import BaseHandler_2
 from trailblazer.store.models import Info
@@ -7,8 +7,8 @@ from trailblazer.store.models import Info
 class UpdateHandler(BaseHandler_2):
     """Class for updating items in the database."""
 
-    def set_latest_update_date(self) -> None:
+    def update_latest_update_date(self) -> None:
         """Update when the database was last updated."""
         info: Info = self._get_query(table=Info).first()
-        info.updated_at: dt.datetime = dt.datetime.now()
+        info.updated_at: datetime = datetime.now()
         self.commit()
