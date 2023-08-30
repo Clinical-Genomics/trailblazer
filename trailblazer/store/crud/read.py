67179c70ed76d92aeb453292e1110d0635bd52ef
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-08-30 10:29:09 +0200

    black

--- trailblazer/store/crud/read.py
+++ trailblazer/store/crud/read.py
@@ -52,13 +52,17 @@ class ReadHandler(BaseHandler_2):
 
     def get_latest_analysis_for_case(self, case_name: str) -> Optional[Analysis]:
         """Return the latest analysis for a case."""
-        return apply_analysis_filter(
-            filter_functions=[
-                AnalysisFilter.FILTER_BY_CASE_NAME,
-            ],
-            analyses=self.get_query(table=Analysis),
-            case_name=case_name,
-        ).order_by(Analysis.started_at).first()
+        return (
+            apply_analysis_filter(
+                filter_functions=[
+                    AnalysisFilter.FILTER_BY_CASE_NAME,
+                ],
+                analyses=self.get_query(table=Analysis),
+                case_name=case_name,
+            )
+            .order_by(Analysis.started_at)
+            .first()
+        )
 
     def get_analysis_with_id(self, analysis_id: int) -> Optional[Analysis]:
         """Get a single analysis by id."""
