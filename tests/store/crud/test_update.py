c22047196242d972895e288afc7100a4a1bb19c6
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-10-12 10:44:41 +0200

    feat(refactor):

--- tests/store/crud/test_update.py
+++ tests/store/crud/test_update.py
@@ -420,6 +420,50 @@ def test_update_tower_run_status(
     assert analysis.status == TrailblazerStatus.PENDING
 
     # WHEN the database is updated once
+    analysis_store.update_tower_run_status(analysis_id=analysis.id)
+
+    # THEN analysis status is updated
+    assert analysis.status == status
+    assert analysis.progress == progress
+
+    # WHEN the database is updated a second time
+    analysis_store.update_run_status(analysis_id=analysis.id)
+
+    # THEN the status is unchanged, and no database errors were raised
+    assert analysis.status == status
+    assert analysis.progress == progress
+
+
+@pytest.mark.parametrize(
+    "case_id, status, progress",
+    [
+        (CaseId.RUNNING, TrailblazerStatus.RUNNING, 0.15),
+        (CaseId.PENDING, TrailblazerStatus.PENDING, 0),
+        (CaseId.COMPLETED, TrailblazerStatus.QC, 1),
+    ],
+)
+def test_update_run_status_using_tower(
+    analysis_store: MockStore,
+    case_id: str,
+    status: str,
+    progress: int,
+    mocker,
+    tower_case_config: Dict[str, dict],
+):
+    """Assess that an analysis status is successfully updated when using NF Tower."""
+
+    # GIVEN Tower API response for an analysis
+    raw_case: dict = tower_case_config.get(case_id)
+    tower_api = MockTowerAPI(workflow_id=raw_case.get("tower_id"))
+    tower_api.mock_query(response_file=raw_case.get("workflow_response_file"))
+    tower_api.mock_tasks_query(response_file=raw_case.get("tasks_response_file"))
+    mocker.patch("trailblazer.store.crud.update.get_tower_api", return_value=tower_api)
+
+    # GIVEN an analysis with pending status
+    analysis: Optional[Analysis] = analysis_store.get_latest_analysis_for_case(case_id=case_id)
+    assert analysis.status == TrailblazerStatus.PENDING
+
+    # WHEN the database is updated once
     analysis_store.update_run_status(analysis_id=analysis.id)
 
     # THEN analysis status is updated
