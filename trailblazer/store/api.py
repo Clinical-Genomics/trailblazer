746f74688d54c6d8ff790f3112b72782dcbb13ca
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-11-06 09:55:18 +0100

    WIP

--- trailblazer/store/api.py
+++ trailblazer/store/api.py
@@ -1,6 +1,7 @@
 """Store backend in Trailblazer."""
 import datetime as dt
 import logging
+from typing import Callable, Dict, List, Optional, Union
 
 import alchy
 import sqlalchemy as sqa
@@ -8,6 +9,10 @@ from alchy import Query
 
 from trailblazer.constants import TrailblazerStatus
 from trailblazer.store.core import CoreHandler
+from trailblazer.store.filters.analyses_filters import (
+    AnalysisFilter,
+    apply_analysis_filter,
+)
 from trailblazer.store.models import Analysis, Model
 
 LOG = logging.getLogger(__name__)
@@ -18,6 +23,29 @@ class BaseHandler(CoreHandler):
 
     def setup(self):
         self.create_all()
+
+    def get_analyses_query_by_search_term_and_is_visible(
+        self,
+        search_term: Optional[str] = None,
+        is_visible: bool = False,
+    ) -> Query:
+        """Return analyses by searchb term qnd is visible."""
+        if not search_term and not is_visible:
+            return
+        filter_map: Dict[Callable, Optional[Union[str, bool]]] = {
+            AnalysisFilter.FILTER_BY_IS_VISIBLE: is_visible,
+            AnalysisFilter.FILTER_BY_SEARCH_TERM: search_term,
+        }
+        filter_functions: List[Callable] = [
+            function for function, supplied_arg in filter_map.items() if supplied_arg
+        ]
+        analyses: Query = apply_analysis_filter(
+            filter_functions=filter_functions,
+            analyses=self.get_query(table=Analysis),
+            search_term=search_term,
+        )
+        #
+        return analyses.order_by(self.Analysis.started_at.desc())
 
     def analyses(
         self,
--- trailblazer/store/base.py
+++ trailblazer/store/base.py
@@ -1,14 +1,10 @@
 from dataclasses import dataclass
-from typing import Callable, Dict, List, Optional, Type, Union
+from typing import Type
 
-from sqlalchemy import desc, func
+from sqlalchemy import func
 from sqlalchemy.orm import Query
 
-from trailblazer.store.filters.analyses_filters import (
-    AnalysisFilter,
-    apply_analysis_filter,
-)
-from trailblazer.store.models import Analysis, Job, Model
+from trailblazer.store.models import Job, Model
 
 
 @dataclass
@@ -28,25 +24,3 @@ class BaseHandler_2:
             Job.name.label("name"),
             func.count(Job.id).label("count"),
         )
-
-    def get_analyses_query_by_search_term_and_is_visible(
-        self,
-        search_term: Optional[str] = None,
-        is_visible: bool = False,
-    ) -> Query:
-        """Return analyses by searchb term qnd is visible."""
-        if not search_term and not is_visible:
-            return
-        filter_map: Dict[Callable, Optional[Union[str, bool]]] = {
-            AnalysisFilter.FILTER_BY_IS_VISIBLE: is_visible,
-            AnalysisFilter.FILTER_BY_SEARCH_TERM: search_term,
-        }
-        filter_functions: List[Callable] = [
-            function for function, supplied_arg in filter_map.items() if supplied_arg
-        ]
-        analyses: Query = apply_analysis_filter(
-            filter_functions=filter_functions,
-            analyses=self.get_query(table=Analysis),
-            search_term=search_term,
-        )
-        return analyses.order_by(desc(Analysis.started_at))
