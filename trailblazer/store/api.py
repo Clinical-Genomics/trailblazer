75bae12e05531342e1852ed44d95e5fcd4fef3fa
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-11-06 10:51:09 +0100

    WIP

--- trailblazer/store/api.py
+++ trailblazer/store/api.py
@@ -19,27 +19,6 @@ class BaseHandler(CoreHandler):
     def setup(self):
         self.create_all()
 
-    def get_analyses_query_by_search_term_and_is_visible(
-        self,
-        search_term: Optional[str] = None,
-        is_visible: bool = False,
-    ) -> Query:
-        """Return analyses by search term qnd is visible."""
-        if not search_term and not is_visible:
-            return
-        filter_map: Dict[Callable, Optional[Union[str, bool]]] = {
-            AnalysisFilter.FILTER_BY_IS_VISIBLE: is_visible,
-            AnalysisFilter.FILTER_BY_SEARCH_TERM: search_term,
-        }
-        filter_functions: List[Callable] = [
-            function for function, supplied_arg in filter_map.items() if supplied_arg
-        ]
-        analyses: Query = apply_analysis_filter(
-            filter_functions=filter_functions,
-            analyses=self.get_query(table=Analysis),
-            search_term=search_term,
-        )
-        return analyses.order_by(Analysis.started_at.desc())
 
 
 class Store(alchy.Manager, BaseHandler):
--- trailblazer/store/base.py
+++ trailblazer/store/base.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
-from typing import Type
+from typing import Type, Dict, Callable, Optional, Union, List
 
 from sqlalchemy import func
 from sqlalchemy.orm import Query
 
-from trailblazer.store.models import Job, Model
+from trailblazer.store.filters.analyses_filters import AnalysisFilter, apply_analysis_filter
+from trailblazer.store.models import Job, Model, Analysis
 
 
 @dataclass
@@ -24,3 +25,26 @@ class BaseHandler_2:
             Job.name.label("name"),
             func.count(Job.id).label("count"),
         )
+
+    def get_analyses_query_by_search_term_and_is_visible(
+        self,
+        search_term: Optional[str] = None,
+        is_visible: bool = False,
+    ) -> Query:
+        """Return analyses by search term qnd is visible."""
+        if not search_term and not is_visible:
+            return
+        filter_map: Dict[Callable, Optional[Union[str, bool]]] = {
+            AnalysisFilter.FILTER_BY_IS_VISIBLE: is_visible,
+            AnalysisFilter.FILTER_BY_SEARCH_TERM: search_term,
+        }
+        filter_functions: List[Callable] = [
+            function for function, supplied_arg in filter_map.items() if supplied_arg
+        ]
+        analyses: Query = apply_analysis_filter(
+            filter_functions=filter_functions,
+            analyses=self.get_query(table=Analysis),
+            search_term=search_term,
+        )
+        return analyses.order_by(Analysis.started_at.desc())
+
