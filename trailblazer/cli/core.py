38d4c342e600ee80063ce8bd9aae8cb2988ca27c
Sebastian Allard <seallards@gmail.com>
2023-11-15 11:33:51 +0100

    Fix syntax

--- trailblazer/cli/core.py
+++ trailblazer/cli/core.py
@@ -32,10 +32,10 @@ class DatabaseResource:
     def __init__(self, db_uri: str):
         self.db_uri = db_uri
 
-    def __enter__(self, _, __, ___):
+    def __enter__(self):
         initialize_database(self.db_uri)
 
-    def __exit__(self):
+    def __exit__(self _, __, ___):
         session: scoped_session = get_session()
         session.remove()
 
