ed42d9e07e2307cd822903c32f2327355a73c26f
seallard <sebastian.allard@scilifelab.se>
2023-11-27 16:29:23 +0100

    Add tests

--- tests/store/test_store_models.py
+++ tests/store/test_store_models.py
@@ -29,9 +29,35 @@ def test_most_recent_failed_job():
     )
 
     # WHEN retrieving the most recent failed job
-    most_recent_failed = analysis.most_recent_failed_job
+    most_recent_failed: Job | None = analysis.most_recent_failed_job
 
     # THEN it should return the job with the status FAILED and the most recent started_at date
     assert most_recent_failed is not None
     assert most_recent_failed.status == SlurmJobStatus.FAILED
     assert most_recent_failed.started_at == datetime(2023, 1, 3)
+
+
+def test_retrieving_non_existing_failed_job():
+    """Test retrieving the most recent failed job, when none has failed."""
+    # GIVEN an Analysis without any failed jobs
+    analysis = Analysis(
+        jobs=[Job(status=SlurmJobStatus.COMPLETED, started_at=datetime(2023, 1, 1))]
+    )
+
+    # WHEN retrieving the most recent failed job
+    most_recent_failed: Job | None = analysis.most_recent_failed_job
+
+    # THEN it should not return any
+    assert most_recent_failed is None
+
+
+def test_most_recent_failed_job_when_no_jobs_exist():
+    """Test retrieving the most recent failed job, when none has failed."""
+    # GIVEN an Analysis without any jobs
+    analysis = Analysis()
+
+    # WHEN retrieving the most recent failed job
+    most_recent_failed: Job | None = analysis.most_recent_failed_job
+
+    # THEN it should not return any
+    assert most_recent_failed is None
