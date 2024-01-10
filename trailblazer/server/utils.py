b820808917d3fd6e2ecf4b4848ede110f1f95604
seallard <sebastian.allard@scilifelab.se>
2024-01-10 13:22:40 +0100

    Fix comments

--- trailblazer/server/utils.py
+++ trailblazer/server/utils.py
@@ -8,11 +8,15 @@ def parse_analyses_request(request: Request) -> AnalysesRequest:
     """Parse a request for retrieving analyses."""
     query_params = {}
     for key in request.args.keys():
-        if key.endswith("[]"):
+        if key_has_list_of_values(key):
             query_params[key[:-2]] = request.args.getlist(key)
         else:
             query_params[key] = request.args.get(key)
     return AnalysesRequest.model_validate(query_params)
+
+
+def key_has_list_of_values(key: str) -> bool:
+    return key.endswith("[]")
 
 
 def parse_analysis_update_request(request: Request) -> AnalysisUpdateRequest:
