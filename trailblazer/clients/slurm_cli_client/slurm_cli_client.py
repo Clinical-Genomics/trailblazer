32679e8b08d65b375eb75ee0047d9529d57fa185
seallard <sebastian.allard@scilifelab.se>
2024-01-26 14:51:13 +0100

    Add job dto

--- trailblazer/clients/slurm_cli_client/slurm_cli_client.py
+++ trailblazer/clients/slurm_cli_client/slurm_cli_client.py
@@ -1,6 +1,9 @@
+from trailblazer.services.slurm.dtos import JobDto
+
+
 class SlurmCLIClient:
     def __init__(self, host: str):
         self.host = host
 
-    def get_job(self, job_id: str) -> dict:
+    def get_job(self, job_id: str) -> JobDto:
         pass
--- trailblazer/services/slurm/slurm_cli_service/slurm_cli_service.py
+++ trailblazer/services/slurm/slurm_cli_service/slurm_cli_service.py
@@ -1,4 +1,5 @@
 from trailblazer.clients.slurm_cli_client.slurm_cli_client import SlurmCLIClient
+from trailblazer.services.slurm.dtos import JobDto
 from trailblazer.services.slurm.slurm_service import SlurmService
 
 
@@ -6,5 +7,5 @@ class SlurmCLIService(SlurmService):
     def __init__(self, client: SlurmCLIClient):
         self.client = client
 
-    def get_job(self, job_id: str) -> None:
+    def get_job(self, job_id: str) -> JobDto:
         self.client.get_job(job_id)
