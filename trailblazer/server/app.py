8a036796b5f4372f21ccc8374945061b0ad2b41c
seallard <sebastian.allard@scilifelab.se>
2024-01-30 17:26:55 +0100

    Cleanup

--- trailblazer/server/app.py
+++ trailblazer/server/app.py
@@ -5,15 +5,10 @@ from flask import Flask
 from flask_cors import CORS
 from flask_reverse_proxy import FlaskReverseProxied
 from sqlalchemy.orm import scoped_session
-from trailblazer.clients.slurm_cli_client.slurm_cli_client import SlurmCLIClient
 
 from trailblazer.server import api, ext
 from trailblazer.server.wiring import setup_dependency_injection
-from trailblazer.services.analysis_service import AnalysisService
-from trailblazer.services.job_service import JobService
-from trailblazer.services.slurm.slurm_cli_service.slurm_cli_service import SlurmCLIService
 from trailblazer.store.database import get_session
-from trailblazer.store.store import Store
 
 app = Flask(__name__)
 setup_dependency_injection()
@@ -26,7 +21,6 @@ TEMPLATES_AUTO_RELOAD = True
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:")
 SQLALCHEMY_POOL_RECYCLE = os.environ.get("SQLALCHEMY_POOL_RECYCLE", 7200)
 SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("FLASK_DEBUG", False)
-ANALYSIS_HOST: str = os.environ.get("ANALYSIS_HOST")
 
 app.config.from_object(__name__)
 
--- trailblazer/services/slurm/slurm_cli_service/slurm_cli_service.py
+++ trailblazer/services/slurm/slurm_cli_service/slurm_cli_service.py
@@ -1,4 +1,6 @@
+from trailblazer.apps.slurm.models import SqueueJob, SqueueResult
 from trailblazer.clients.slurm_cli_client.slurm_cli_client import SlurmCLIClient
+from trailblazer.clients.slurm_cli_client.utils import create_job_info_dto
 from trailblazer.services.slurm.dtos import SlurmJobInfo
 from trailblazer.services.slurm.slurm_service import SlurmService
 
@@ -8,4 +10,6 @@ class SlurmCLIService(SlurmService):
         self.client = client
 
     def get_job_info(self, job_id: int) -> SlurmJobInfo:
-        return self.client.get_job_info(job_id)
+        queue: SqueueResult = self.client.get_slurm_queue(str(job_id))
+        job: SqueueJob = queue.jobs[0]
+        return create_job_info_dto(job)
