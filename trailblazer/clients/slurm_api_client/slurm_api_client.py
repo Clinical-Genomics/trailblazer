4be8a95ba265312751cc5967f1368f105e193407
seallard <sebastian.allard@scilifelab.se>
2024-01-31 14:25:19 +0100

    Pretty print job info

--- trailblazer/clients/slurm_api_client/slurm_api_client.py
+++ trailblazer/clients/slurm_api_client/slurm_api_client.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 from trailblazer.clients.slurm_api_client.dto import (
@@ -16,7 +17,7 @@ class SlurmApiClient:
         endpoint: str = f"{self.base_url}/slurm/v0.0.40/job/{job_id}"
         response = requests.get(endpoint, headers=self.headers)
         response.raise_for_status()
-        print(response.json())
+        print(json.dumps(response.json(), indent=4))
         return SlurmJobResponse.model_validate(response.json())
 
     def get_jobs(self) -> SlurmJobsResponse:
