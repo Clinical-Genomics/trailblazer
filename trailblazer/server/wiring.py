ef3bfd631f8afca2da3d138b75a7f2dc56531797
seallard <sebastian.allard@scilifelab.se>
2024-02-02 14:05:44 +0100

    Fix circular import

--- trailblazer/server/wiring.py
+++ trailblazer/server/wiring.py
@@ -1,10 +1,10 @@
 from trailblazer.containers import Container
-import trailblazer.server.api
-import trailblazer.cli.core
 
 container = Container()
 
 
 def setup_dependency_injection() -> Container:
+    import trailblazer.server.api
+    import trailblazer.cli.core
     container.wire(modules=[trailblazer.server.api, trailblazer.cli.core])
     return container
