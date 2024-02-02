254b77ae3a587a0728955574814e11d681d82ee1
seallard <sebastian.allard@scilifelab.se>
2024-02-02 14:07:21 +0100

    Formatting

--- trailblazer/server/wiring.py
+++ trailblazer/server/wiring.py
@@ -6,5 +6,6 @@ container = Container()
 def setup_dependency_injection() -> Container:
     import trailblazer.server.api
     import trailblazer.cli.core
+
     container.wire(modules=[trailblazer.server.api, trailblazer.cli.core])
     return container
