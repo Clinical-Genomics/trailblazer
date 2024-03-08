3423a6db7441f80bc62fe925ddd00ac6a0f8a2a1
Sebastian Allard <seallards@gmail.com>
2024-03-08 14:37:29 +0100

    Remove unnecessary exception

--- trailblazer/services/authentication_service/authentication_service.py
+++ trailblazer/services/authentication_service/authentication_service.py
@@ -38,11 +38,6 @@ class AuthenticationService:
     def refresh_access_token(self, user_id: int) -> str:
         """Refresh the users access token."""
         user: User = self.store.get_user_by_id(user_id)
-
-        if not user:
-            raise UserNotFoundError
-
         refresh_token: str = self.encryption_service.decrypt(user.refresh_token)
         access_token: str = self.google_oauth_client.get_access_token(refresh_token)
-
         return access_token
