b5ae31cb70251037cc9fa323db6c15f5730790f6
Sebastian Allard <seallards@gmail.com>
2024-03-08 11:04:05 +0100

    Formatting

--- trailblazer/server/api.py
+++ trailblazer/server/api.py
@@ -50,7 +50,7 @@ blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 @blueprint.before_request
 def before_request():
     """Authentication that is run before processing requests to the application"""
-    if request.endpoint == 'api.authenticate':
+    if request.endpoint == "api.authenticate":
         return
     if request.method == "OPTIONS":
         return make_response(jsonify(ok=True), 204)
