ee4d967185a94f266225e39bdad4751a8a5ec9e3
islean <isak.ohlsson.angnell@gmail.com>
2024-03-14 14:55:15 +0100

    Address comments

--- trailblazer/dto/analyses_response.py
+++ trailblazer/dto/analyses_response.py
@@ -18,7 +18,7 @@ class Analysis(BaseModel):
     comment: str | None = None
     completed_at: datetime | None = None
     config_path: str | None = None
-    delivered: bool | None = None
+    is_delivered: bool | None = None
     workflow: str | None = None
     failed_job: Job | None = None
     id: int
