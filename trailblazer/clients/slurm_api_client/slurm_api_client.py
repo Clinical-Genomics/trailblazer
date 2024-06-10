3e1c56ff1e378f41b953d35b98a5bd7835eaa39e
seallard <sebastian.allard@scilifelab.se>
2024-06-10 09:56:30 +0200

    Remove unused fields

--- trailblazer/clients/slurm_api_client/slurm_api_client.py
+++ trailblazer/clients/slurm_api_client/slurm_api_client.py
@@ -17,19 +17,13 @@ class SlurmAPIClient:
         self.headers = {"X-SLURM-USER-NAME": user_name, "X-SLURM-USER-TOKEN": access_token}
 
     def get_job(self, job_id: str) -> SlurmJobResponse:
-        LOG.info(f"Getting job {job_id}")
-        LOG.info(f"Headers: {self.headers}")
-        LOG.info(f"Base URL: {self.base_url}")
         endpoint: str = f"{self.base_url}/slurm/v0.0.40/job/{job_id}"
-        LOG.info(f"Endpoint: {endpoint}")
         try:
             response = requests.get(endpoint, headers=self.headers)
             response.raise_for_status()
         except requests.exceptions.HTTPError as e:
-            LOG.error(f"Error getting job {job_id}: {e.response.text}")
-            LOG.error(f"Response: {response.content}")
+            LOG.error(f"Error getting job {job_id}: {e.response.content}")
             raise SlurmAPIClientError(e)
-        LOG.debug(f"Response: {response.json()}")
         return SlurmJobResponse.model_validate(response.json())
 
     def get_jobs(self) -> SlurmJobsResponse:
