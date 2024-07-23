1ece493298ee9aa57905d2189ba5c80f836d4cdc
seallard <sebastian.allard@scilifelab.se>
2024-07-23 17:56:16 +0200

    Remove weird stuff

--- trailblazer/services/tower/tower_api_service.py
+++ trailblazer/services/tower/tower_api_service.py
@@ -21,20 +21,10 @@ LOG = logging.getLogger(__name__)
 class TowerAPIService:
     """Class communicating with NF tower regarding a given analysis (workflow)."""
 
-    def __init__(self, workflow_id: str, dry_run: bool = False):
-        self.workflow_id: str = workflow_id
+    def __init__(self, tower_client: TowerApiClient, dry_run: bool = False) -> None:
         self.dry_run: bool = dry_run
-        self._tower_client = None
-        self._response = None
-        self._tasks_response = None
-
-    @property
-    def tower_client(self) -> TowerApiClient:
-        """Returns a NF Tower client."""
-        if not self._tower_client:
-            self._tower_client = TowerApiClient(workflow_id=self.workflow_id)
-        return self._tower_client
-
+        self.tower_client = tower_client
+ 
     @property
     def response(self) -> TowerWorkflowResponse:
         """Returns a workflow response containing general information about an analysis."""
@@ -128,21 +118,3 @@ class TowerAPIService:
     def cancel(self) -> None:
         """Cancel a workflow."""
         self.tower_client.cancel_workflow()
-
-
-def _validate_tower_api_client_requirements(tower_api: TowerAPIService) -> bool:
-    """Raises:
-    TowerRequirementsError when failing meeting Tower mandatory requirement"""
-    if not tower_api.tower_client.meets_requirements:
-        raise TowerRequirementsError("Could not initialize Tower API due to missing requirements")
-    return True
-
-
-def get_tower_api(config_file_path: str, case_id: str) -> TowerAPIService | None:
-    """Return Tower API. Currently only one tower ID is supported."""
-    workflow_id: int = ReadFile.get_content_from_file(
-        file_format=FileFormat.YAML, file_path=Path(config_file_path)
-    ).get(case_id)[-1]
-    tower_api = TowerAPIService(workflow_id=str(workflow_id))
-    if _validate_tower_api_client_requirements(tower_api=tower_api):
-        return tower_api
