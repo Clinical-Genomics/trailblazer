262c8a5184076b108002a9e56f3787400b58ed30
Sebastian Allard <seallards@gmail.com>
2024-07-24 15:04:43 +0200

    Use abbreviation (#465)

--- trailblazer/store/models.py
+++ trailblazer/store/models.py
@@ -112,8 +112,8 @@ class Analysis(Model):
         return [job for job in self.jobs if job.job_type == JobType.UPLOAD]
 
     @property
-    def delivered_by(self) -> User | None:
-        return self.delivery.user.name if self.delivery else None
+    def delivered_by(self) -> str | None:
+        return self.delivery.user.abbreviation if self.delivery else None
 
     @property
     def delivered_date(self) -> datetime.datetime | None:
