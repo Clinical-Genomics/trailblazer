671833554b2f07b4194bbd7b7275e6c300c4d4e9
seallard <sebastian.allard@scilifelab.se>
2024-08-07 11:12:30 +0200

    Add tower fixture stubs

--- tests/clients/conftest.py
+++ tests/clients/conftest.py
@@ -2,14 +2,24 @@ import pytest
 import requests_mock
 
 from trailblazer.clients.slurm_api_client.slurm_api_client import SlurmAPIClient
+from trailblazer.clients.tower.tower_client import TowerAPIClient
 
 
 @pytest.fixture
-def client() -> SlurmAPIClient:
+def slurm_client() -> SlurmAPIClient:
     return SlurmAPIClient(
         base_url="https://slurm.example.com",
         access_token="access_token",
         user_name="user_name",
+    )
+
+
+@pytest.fixture
+def tower_client():
+    return TowerAPIClient(
+        base_url="https://tower",
+        access_token="token",
+        workspace_id="workspace_id",
     )
 
 
@@ -22,3 +32,23 @@ def mock_request():
 @pytest.fixture
 def jobs_response() -> dict:
     return {"jobs": [{"name": "job 1"}, {"name": "job 2"}]}
+
+
+@pytest.fixture
+def tower_empty_task_response() -> dict:
+    pass
+
+
+@pytest.fixture
+def tower_pending_tasks_response() -> dict:
+    pass
+
+
+@pytest.fixture
+def tower_running_tasks_response() -> dict:
+    pass
+
+
+@pytest.fixture
+def tower_completed_tasks_response() -> dict:
+    pass
