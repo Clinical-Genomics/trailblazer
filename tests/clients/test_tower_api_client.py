351a554e7700c53f732035f8b6b264f74496b2bf
seallard <sebastian.allard@scilifelab.se>
2024-08-07 11:46:08 +0200

    Add completed tasks test

--- tests/clients/test_tower_api_client.py
+++ tests/clients/test_tower_api_client.py
@@ -36,3 +36,21 @@ def test_running_tasks_response_is_accepted(
 
     # THEN the response should have a total of tasks
     assert response.total
+
+
+def test_completed_tasks_response_is_accepted(
+    tower_client: TowerAPIClient,
+    tower_completed_tasks_response: dict,
+    mock_request: Mocker,
+):
+    # GIVEN a workflow response with completed tasks
+    mock_request.get("https://tower/workflow/1/tasks", json=tower_completed_tasks_response)
+
+    # WHEN fetching the tasks for a workflow
+    response = tower_client.get_tasks(workflow_id="1")
+
+    # THEN the response should contain completed tasks
+    assert response.tasks
+
+    # THEN the response should have a total of tasks
+    assert response.total
