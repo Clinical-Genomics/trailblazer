bd9fe4bc8dfaa1b0e0b8ad80cedaab031099e2ff
seallard <sebastian.allard@scilifelab.se>
2024-08-26 10:52:05 +0200

    Add tower service error handler

--- trailblazer/services/tower/tower_api_service.py
+++ trailblazer/services/tower/tower_api_service.py
@@ -1,6 +1,7 @@
 from trailblazer.clients.tower.models import TowerTasksResponse
 from trailblazer.clients.tower.tower_client import TowerAPIClient
 from trailblazer.constants import TOWER_WORKFLOW_STATUS, TrailblazerStatus
+from trailblazer.services.tower.error_handler import handle_errors
 from trailblazer.services.tower.utils import create_job_from_tower_task, get_tower_workflow_id
 from trailblazer.store.models import Analysis, Job
 from trailblazer.store.store import Store
@@ -13,6 +14,7 @@ class TowerAPIService:
         self.client = client
         self.store = store
 
+    @handle_errors
     def update_jobs(self, analysis_id: int) -> list[Job]:
         analysis: Analysis = self.store.get_analysis_with_id(analysis_id)
         workflow_id: str = get_tower_workflow_id(analysis)
@@ -20,12 +22,14 @@ class TowerAPIService:
         jobs: list[Job] = [create_job_from_tower_task(task) for task in response.get_tasks()]
         self.store.replace_jobs(analysis_id=analysis_id, jobs=jobs)
 
+    @handle_errors
     def cancel_jobs(self, analysis_id: int) -> None:
         analysis: Analysis = self.store.get_analysis_with_id(analysis_id)
         workflow_id: str = get_tower_workflow_id(analysis)
         self.client.cancel_workflow(workflow_id)
         self.update_jobs(analysis_id)
 
+    @handle_errors
     def get_status(self, workflow_id: str) -> TrailblazerStatus:
         response = self.client.get_workflow(workflow_id)
         status = TOWER_WORKFLOW_STATUS.get(response.workflow.status, TrailblazerStatus.ERROR)
