8dd64f2433141b81f036aeefc185545c372821cd
seallard <sebastian.allard@scilifelab.se>
2024-08-30 14:13:54 +0200

    Remove tower workflow file interactions

--- trailblazer/services/tower/tower_api_service.py
+++ trailblazer/services/tower/tower_api_service.py
@@ -17,8 +17,7 @@ class TowerAPIService:
     @handle_errors
     def update_jobs(self, analysis_id: int) -> list[Job]:
         analysis: Analysis = self.store.get_analysis_with_id(analysis_id)
-        workflow_id: str = get_tower_workflow_id(analysis)
-        response: TowerTasksResponse = self.client.get_tasks(workflow_id)
+        response: TowerTasksResponse = self.client.get_tasks(analysis.tower_workflow_id)
         jobs: list[Job] = [create_job_from_tower_task(task) for task in response.get_tasks()]
         self.store.replace_jobs(analysis_id=analysis_id, jobs=jobs)
 
@@ -31,8 +30,7 @@ class TowerAPIService:
     @handle_errors
     def get_status(self, analysis_id: int) -> TrailblazerStatus:
         analysis: Analysis = self.store.get_analysis_with_id(analysis_id)
-        workflow_id: str = get_tower_workflow_id(analysis)
-        response = self.client.get_workflow(workflow_id)
+        response = self.client.get_workflow(analysis.tower_workflow_id)
         status = TOWER_WORKFLOW_STATUS.get(response.workflow.status, TrailblazerStatus.ERROR)
         if status == TrailblazerStatus.COMPLETED:
             return TrailblazerStatus.QC
--- trailblazer/services/tower/utils.py
+++ trailblazer/services/tower/utils.py
@@ -1,14 +1,5 @@
-from pathlib import Path
 from trailblazer.clients.tower.models import TowerTask
-from trailblazer.constants import FileFormat
-from trailblazer.io.controller import ReadFile
-from trailblazer.store.models import Analysis, Job
-
-
-def get_tower_workflow_id(analysis: Analysis) -> str:
-    file = Path(analysis.config_path)
-    content: dict = ReadFile.get_content_from_file(file_format=FileFormat.YAML, file_path=file)
-    return content.get(analysis.case_id)[-1]
+from trailblazer.store.models import Job
 
 
 def create_job_from_tower_task(task: TowerTask) -> Job:
