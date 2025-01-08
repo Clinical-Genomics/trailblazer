c5d13a3ccba753e74f2c7d9dc1e0c05cb984da8f
Christian Oertlin <c.oertlin@gmail.com>
2025-01-08 13:52:11 +0100

    fix: make certs url v1

--- trailblazer/containers.py
+++ trailblazer/containers.py
@@ -20,7 +20,6 @@ from trailblazer.store.store import Store
 
 
 class Container(containers.DeclarativeContainer):
-    client_certs_url: str | None = os.environ.get("CLIENT_CERTS_URL")
     slurm_host: str | None = os.environ.get("ANALYSIS_HOST")
     oauth_client_id: str | None = os.environ.get("GOOGLE_CLIENT_ID")
     oauth_client_secret: str | None = os.environ.get("GOOGLE_CLIENT_SECRET")
@@ -86,7 +85,7 @@ class Container(containers.DeclarativeContainer):
         UserVerificationService,
         store=store,
         google_client_id=oauth_client_id,
-        client_certs_url=client_certs_url,
+        google_oauth_base_url=google_oauth_base_url,
     )
     auth_service = providers.Singleton(
         AuthenticationService,
