ec9dc8b4a99c8aee448a328fbba16e3ae3dda41f
ahdamin <ahmed.amin@regionstockholm.se>
2025-10-24 17:51:01 +0200

    re-add headers to the fixtures

--- tests/apps/slurm/conftest.py
+++ tests/apps/slurm/conftest.py
@@ -4,10 +4,12 @@ import pytest
 @pytest.fixture(scope="session")
 def squeue_stream_pending_job() -> str:
     """Return a squeue output stream."""
-    return """690990,gatk_genotypegvcfs6,PENDING,10:00:00,00:00:00,2020-10-22T11:42:00"""
+    return """JOBID,NAME,STATE,TIME_LIMIT,TIME,START_TIME
+690990,gatk_genotypegvcfs6,PENDING,10:00:00,00:00:00,2020-10-22T11:42:00"""
 
 
 @pytest.fixture(scope="session")
 def squeue_stream_pending_job_not_started() -> str:
     """Return a squeue output stream for a not started job."""
-    return """690990,gatk_genotypegvcfs6,PENDING,10:00:00,00:00:00,2020-10-22T11:42:00"""
+    return """JOBID,NAME,STATE,TIME_LIMIT,TIME,START_TIME
+690990,gatk_genotypegvcfs6,PENDING,10:00:00,00:00:00,2020-10-22T11:42:00"""
