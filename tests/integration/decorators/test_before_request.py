e128d85f417081544aa70731395afd0af3fb7b70
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-04-01 13:31:20 +0200

    Use app instead of blueprint

--- tests/integration/decorators/test_before_request.py
+++ tests/integration/decorators/test_before_request.py
@@ -1,12 +1,13 @@
 from unittest.mock import create_autospec
 
 import pytest
-from flask import Flask, g
+from flask import g
 from flask.testing import FlaskClient
 from pytest_mock import MockerFixture
 from werkzeug.test import TestResponse
 
-from trailblazer.server.api import blueprint
+from trailblazer.server.api import before_request
+from trailblazer.server.app import app
 from trailblazer.server.wiring import setup_dependency_injection
 from trailblazer.services.user_verification_service.user_verification_service import (
     UserVerificationService,
@@ -16,23 +17,18 @@ from trailblazer.store.models import User
 container = setup_dependency_injection()
 
 
+app.before_request(before_request)
+
+
+@app.route("/test-before-request")
+def test_endpoint():
+    return f"Current user: {g.current_user.name}", 200
+
+
 @pytest.fixture
-def before_request_app():
-    app = Flask("test_app")
+def client() -> FlaskClient:
     app.config["TESTING"] = True
-
-    @blueprint.route("/test-before-request")
-    def test_endpoint():
-        return f"Current user: {g.current_user.name}", 200
-
-    app.register_blueprint(blueprint)
-
-    return app
-
-
-@pytest.fixture
-def client(before_request_app: Flask) -> FlaskClient:
-    return before_request_app.test_client()
+    return app.test_client()
 
 
 def test_before_request_with_authorization_header(client: FlaskClient, mocker: MockerFixture):
@@ -48,7 +44,7 @@ def test_before_request_with_authorization_header(client: FlaskClient, mocker: M
     # WHEN an endpoint decorated with before_request is called
     with container.user_verification_service.override(user_verification_service):
         response: TestResponse = client.get(
-            "/api/v1/test-before-request", headers={"Authorization": valid_authorization}
+            "/test-before-request", headers={"Authorization": valid_authorization}
         )
         # THEN the current user was available in that endpoint
         assert response.text == "Current user: Logged In"
